caf71a493cbd17588539796614b0fd872041128c
novailoveyou <novailoveyoux@gmail.com>
2021-05-15 11:17:40 +0300

    improved funcs

--- frontend/app.js
+++ frontend/app.js
@@ -1,72 +1,96 @@
 const VarsCtrl = (() => {
-	const getKeys = () => {
-		return {
-			vapidPublic:
-				'BMLm6sDkLNPTbWX_7TwI-I1-9J5tEVtbeq9Iph_Fue5KCILb0EP4xsmLz8SvEmG2mDeplGxJK-4lC1W3m9yvqk0'
-		}
-	}
+  const getKeys = () => {
+    return {
+      vapidPublic:
+        'BMLm6sDkLNPTbWX_7TwI-I1-9J5tEVtbeq9Iph_Fue5KCILb0EP4xsmLz8SvEmG2mDeplGxJK-4lC1W3m9yvqk0'
+    }
+  }
 
-	return {
-		keys: getKeys()
-	}
+  return {
+    keys: getKeys()
+  }
 })()
 
 const SwCtrl = (() => {
-	const vapidPublicKey = VarsCtrl.keys.vapidPublic
+  const vapidPublicKey = VarsCtrl.keys.vapidPublic
 
-	const registerSw = async (path = null, scope = '/') => {
-		return await navigator.serviceWorker.register(path, {
-			scope
-		})
-	}
+  const registerSw = async ({ path = null, scope = '/' }) => {
+    return await navigator.serviceWorker.register(path, {
+      scope
+    })
+  }
 
-	const registerPush = async () => {
-		const register = await registerSw('/sw.js')
-		return await register.pushManager.subscribe({
-			userVisibleOnly: true,
-			applicationServerKey: vapidPublicKey
-		})
-	}
+  const registerPush = async ({
+    registerFunc = null,
+    path = null,
+    scope,
+    key = null
+  }) => {
+    const register = await registerFunc({ path, scope })
+    return await register.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: key
+    })
+  }
 
-	const sendPush = async () => {
-		const output = await fetch('/subscribe', {
-			method: 'POST',
-			body: JSON.stringify(await registerPush()),
-			headers: {
-				'content-type': 'application/json'
-			}
-		})
-		console.log('Push sent')
-		return output
-	}
+  const sendPush = async ({
+    registerFunc = null,
+    path = null,
+    scope,
+    key = null,
+    pushFunc = null,
+    route = null
+  }) => {
+    const body = JSON.stringify(
+      await pushFunc({
+        registerFunc,
+        path,
+        scope,
+        key
+      })
+    )
+    return await fetch(route, {
+      method: 'POST',
+      body,
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+  }
 
-	const sw = async () => {
-		try {
-			sendPush()
-		} catch (err) {
-			console.error(`Service Worker: Error: ${err}`)
-		}
-	}
+  const sw = async () => {
+    try {
+      navigator.serviceWorker &&
+        sendPush({
+          registerFunc: registerSw,
+          path: './sw.js',
+          key: vapidPublicKey,
+          pushFunc: registerPush,
+          route: '/subscribe'
+        })
+    } catch (err) {
+      console.error(`Service Worker: Error: ${err}`)
+    }
+  }
 
-	return {
-		sw
-	}
+  return {
+    sw
+  }
 })()
 
 const UICtrl = (() => {
-	const sw = SwCtrl.sw()
-	const loadEvtListeners = () =>
-		navigator.serviceWorker && window.addEventListener('load', sw)
+  const sw = SwCtrl.sw()
+  const loadEvtListeners = () => window.addEventListener('load', sw)
 
-	return {
-		loadEvtListeners
-	}
+  return {
+    loadEvtListeners
+  }
 })()
 
 const App = (UICtrl => {
-	const init = () => UICtrl.loadEvtListeners()
+  const init = () => UICtrl.loadEvtListeners()
 
-	return { init }
+  return { init }
 })(UICtrl)
 
 App.init()
